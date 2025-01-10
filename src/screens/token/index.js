497963f99f13a69336844149fdcc65aecd9fad5e
macs1756 <kindrativmaksum312@gmail.com>
2025-01-10 14:35:40 +0200

    test

--- src/screens/token/index.js
+++ src/screens/token/index.js
@@ -5,32 +5,29 @@ import * as Notifications from 'expo-notifications';
 const PushNotificationComponent = () => {
   const [pushToken, setPushToken] = useState(null);
 
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  async function getDeviceToken() {
+    const token = await messaging().getToken();
+    console.log('Device Token:', token);
+    setPushToken(token)
+    return token;
+  }
+
   useEffect(() => {
-    // Request permission for notifications directly with expo-notifications
-    const getNotificationPermission = async () => {
-      const { status } = await Notifications.requestPermissionsAsync(); // Request permission here
-      if (status === 'granted') {
-        // Get the token for push notifications
-        const token = await Notifications.getExpoPushTokenAsync();
-        setPushToken(token.data);
-        console.log('Expo Push Token:', token.data);
-      } else {
-        console.log('Permission not granted for push notifications');
-      }
-    };
+    requestUserPermission()
+    getDeviceToken()
+  }, [])
 
-    getNotificationPermission();
-
-    // Optionally: Handle received notifications
-    Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification received:', notification);
-    });
-
-    // Optionally: Handle notification response
-    Notifications.addNotificationResponseReceivedListener(response => {
-      console.log('Notification response:', response);
-    });
-  }, []);
 
   return (
     <View>
